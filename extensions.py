ed247eae165518801c2e891a244314786f0bebb6
kharisovva <166054747+kharisovva@users.noreply.github.com>
2024-11-28 16:09:35 +0500

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,29 @@
+# вопросы викторины с ответами
+questions_data = {
+    'Твоя самая любимая стихия?': {'Воздух': 'answ_air', 'Вода': 'answ_water', 'Земля': 'answ_earth', 'Огонь': 'answ_fire'},
+    'Ты ведешь расслабленный образ жизни и предпочитаешь не торопиться?': {'Да': 'answ_yes', 'Нет': 'answ_no'},
+    'Когда ты достигаешь наибольшего пика активности?': {'Утром': 'answ_morning', 'Днем': 'answ_day', 'Вечером': 'answ_evening',
+                                                         'Ночью': 'answ_night'},
+    'Что предпочитаешь есть?': {'Мясо/рыбу': 'answ_meat', 'Овощи/салаты': 'answ_veg', 'Фаст-фуд': 'answ_burger'},
+    'Какое качество развито в тебе больше всего?': {'Дружба': 'answ_friendship', 'Мудрость': 'answ_wise',
+                                                    'Энергичность': 'answ_energy'},
+    'Ты общительный человек?': {'Да': 'answ_talk', 'Не особо': 'answ_alone', 'Общаюсь по настроению': 'answ_moody'},
+    'Привлекают ли тебя путешествия и постоянное движение?': {'Я редко путешествую': 'answ_rarely',
+                                                              'Предпочитаю быть дома': 'answ_home', 'Привлекают': 'answ_often'},
+    'Чего с твоей жизни больше всего?': {'Беззаботности': 'answ_careless', 'Любопытства': 'answ_curious', 'Риска': 'answ_risk'},
+    'Чем ты руководствуешься при принятии решений?': {'Сердцем': 'answ_heart', 'Разумом': 'answ_mind', 'Интуицией': 'answ_intuition',
+                                                      'Поступаю наобум': 'answ_destiny'}
+}
+
+answers_data = {
+    'answ_air': 1, 'answ_water': 2, 'answ_earth': 3, 'answ_fire': 4,
+    'answ_yes': 4, 'answ_no': 1,
+    'answ_morning': 1, 'answ_day': 2, 'answ_evening': 3, 'answ_night': 4,
+    'answ_meat': 1, 'answ_veg': 2, 'answ_burger': 3,
+    'answ_friendship': 4, 'answ_wise': 3, 'answ_energy': 1,
+    'answ_talk': 1, 'answ_alone': 2, 'answ_moody': 3,
+    'answ_rarely': 4, 'answ_home': 3, 'answ_often': 1,
+    'answ_careless': 4, 'answ_curious': 3, 'answ_risk': 1,
+    'answ_heart': 3, 'answ_mind': 2, 'answ_intuition': 1, 'answ_destiny': 4
+}
+
